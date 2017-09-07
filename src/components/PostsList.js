94c21724267ec341403984dbde5fa9f015444206
Angel Muelas <angel.muelas@gmail.com>
2017-09-07 07:40:35 +0200

    feat: added components for posts and postsList

---
+++ src/components/PostsList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Post from './Post';
+
+function PostsList(props) {
+  const { posts, category } = props;
+
+  return (
+    <div>
+      <div>
+        <h3>Posts</h3>
+        <hr />
+        {posts && category ? posts.filter(post =>
+          {return post.category === category}).map((post) =>
+            <Post key={post.id} post={post} />) : posts ? posts.map((post) =>
+              <Post key={post.id} post={post} />) : ''}
+      </div>
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    posts: state.postData.posts
+  }
+}
+
+export default connect(mapStateToProps)(PostsList);
