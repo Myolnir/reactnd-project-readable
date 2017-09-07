5b6e0222bc13d5df4adb902382eda537f0150a3c
Angel Muelas <angel.muelas@gmail.com>
2017-09-07 07:34:39 +0200

    feat: added Header component and reducer

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Header from './Header';
+import * as actions from '../actions';
+import '../App.css';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+    this.props.getCategories();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
