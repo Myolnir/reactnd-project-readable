9f86ecb83ecfd84ecd49ca4ca0933f1e902ae735
Angel Muelas <angel.muelas@gmail.com>
2017-09-07 07:38:32 +0200

    feat: added action creators, redux thunk and wrap with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+import reducer from '../src/reducers';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(ReduxThunk)
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
