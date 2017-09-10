ee7b3730a8e8ed4aad4d51a62714b24727cb445e
Angel Muelas <angel.muelas@gmail.com>
2017-09-10 12:32:01 +0200

    chore: refactor

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,31 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
 import Post from './Post';
 
-class PostsList extends Component {
-  componentDidUpdate() {
-    actions.getComments(this.props.posts);
-  }
+function PostsList(props) {
+  const { posts, category } = props;
 
-  render() {
-    const { posts, category } = this.props;
-
-    return (
-      <div>
-        <div>
-          <h3>Posts</h3>
-          <hr />
-          {posts && category ? posts.filter(post =>
-            {return post.category === category}).map((post) =>
-              <Post key={post.id} post={post} />) : posts ? posts.map((post) =>
-                <Post key={post.id} post={post} />) : <h4>There are no Post for { category } at this time.</h4>}
-        </div>
-      </div>
-    )
-  }
+  return (
+    <div>
+      {posts && category ? posts.filter(post =>
+        {return post.category === category}).map((post) =>
+          <Post key={post.id} post={post} />) : posts ? posts.map((post) =>
+            <Post key={post.id} post={post} />) : <h4>There are no Post for { category } at this time.</h4>}
+    </div>
+  )
 }
+
 
 function mapStateToProps(state) {
   return {
